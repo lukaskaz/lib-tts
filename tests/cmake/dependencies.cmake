afd70bc0951d13c83d20808420f76a3f008c2d16
lukaskaz <lukask84@o2.pl>
2024-07-04 23:51:41 +0200

    Adding factory for TextToVoice (#5)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- tests/cmake/dependencies.cmake
+++ tests/cmake/dependencies.cmake
@@ -31,16 +31,17 @@ EXTERNALPROJECT_ADD(
   libshellcmd
   GIT_REPOSITORY    https://github.com/lukaskaz/lib-shellcmd.git
   GIT_TAG           main
-  PATCH_COMMAND     ""
+  PATCH_COMMAND     ${patching_cmd}
   PREFIX            libshellcmd-workspace
   SOURCE_DIR        ${source_dir}
   BINARY_DIR        ${build_dir}
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND     ""
+  CONFIGURE_COMMAND mkdir /${build_dir}/build &> /dev/null
+  BUILD_COMMAND     cd ${build_dir}/build && cmake -D BUILD_SHARED_LIBS=ON
+                    ${source_dir} && make -j 4
   UPDATE_COMMAND    ""
   INSTALL_COMMAND   "" 
   TEST_COMMAND      ""
 )
 
 include_directories(${source_dir}/inc)
-# link_directories(${build_dir}/build)
+link_directories(${build_dir}/build)
