9e6558d3379ffb5d760162ee02ff11246dd3c46b
Lukasz Kazmierczak <lukask84@o2.pl>
2024-07-04 15:53:33 +0200

    Adding debug flags for cmake
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -5,8 +5,9 @@ set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} \
   -flto"
 )
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-psabi")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g3 -ggdb")
 
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-psabi")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
   -pedantic \
   -Wall \
