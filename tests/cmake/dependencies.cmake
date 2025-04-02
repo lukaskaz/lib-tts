2d1002b85d253e930053b419e419512d94714c06
Lukasz Kazmierczak <lukask84@o2.pl>
2025-04-02 18:03:17 +0200

    Adding logger system for debug traces, v5
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- tests/cmake/dependencies.cmake
+++ tests/cmake/dependencies.cmake
@@ -26,6 +26,28 @@ include_directories(${source_dir}/googletest/include)
 include_directories(${source_dir}/googlemock/include)
 link_directories(${build_dir}/lib)
 
+set(source_dir "${CMAKE_BINARY_DIR}/liblogger-src")
+set(build_dir "${CMAKE_BINARY_DIR}/liblogger-build")
+
+EXTERNALPROJECT_ADD(
+  liblogger
+  GIT_REPOSITORY    https://github.com/lukaskaz/lib-logger.git
+  GIT_TAG           main
+  PATCH_COMMAND     ""
+  PREFIX            liblogger-workspace
+  SOURCE_DIR        ${source_dir}
+  BINARY_DIR        ${build_dir}
+  CONFIGURE_COMMAND mkdir /${build_dir}/build &> /dev/null
+  BUILD_COMMAND     cd ${build_dir}/build && cmake -D BUILD_SHARED_LIBS=ON
+                    ${source_dir} && make
+  UPDATE_COMMAND    ""
+  INSTALL_COMMAND   "" 
+  TEST_COMMAND      ""
+)
+
+include_directories(${source_dir}/inc)
+link_directories(${build_dir}/build)
+
 set(source_dir "${CMAKE_CURRENT_BINARY_DIR}/libshellcmd-src")
 set(build_dir "${CMAKE_CURRENT_BINARY_DIR}/libshellcmd-build")
 
