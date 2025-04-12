94e31587fbc9816b70c4b49c3c52b88ffb10e383
lukaskaz <lukask84@o2.pl>
2025-04-12 14:47:33 +0200

    Adding wait routine for async speak calls (#18)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- inc/tts/interfaces/texttovoice.hpp
+++ inc/tts/interfaces/texttovoice.hpp
@@ -31,6 +31,7 @@ class TextToVoiceIf
     virtual bool speak(const std::string&, const voice_t&) = 0;
     virtual bool speakasync(const std::string&) = 0;
     virtual bool speakasync(const std::string&, const voice_t&) = 0;
+    virtual bool waitspoken() = 0;
     virtual voice_t getvoice() = 0;
     virtual void setvoice(const voice_t&) = 0;
     static void kill();
